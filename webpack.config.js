ef4e7b73cea9ed338b7efbcaf486ad35b18d5070
francesco-strazzullo <me@francescostrazzullo.info>
2016-05-14 17:21:21 +0200

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+    './src/index' // Your appʼs entry point
+  ],
+  output: {
+    path: path.resolve(__dirname, './build'),
+    publicPath: '/build/',
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['react-hot', 'babel']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|svg)$/,
+        loader: 'url'
+      }
+    ]
+  }
+};
